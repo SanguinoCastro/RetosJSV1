e8bfab05dc1f27749388c90fe196ff46a74a47d5
Eduard <eduardsanguinocastro@gmail.com>
2023-01-04 16:03:26 +0100

    Se ha realizado y comprobado el reto nº 4

--- Reto 4/JS/main.js
+++ Reto 4/JS/main.js
@@ -1,15 +1,9 @@
-//Creamos las variables númericas con valor fijo
-var numero1 = 15;
-var numero2 = 15;
+//Creamos la variable númerica cuyo valor se pedirá al usuario
+var R = Number(prompt('Introduce el radio de la circunferencia a calcular'));
 
-//hacemos una comparación de condición en dos pasos:
-if (numero1 == numero2){  
-    console.log('Los dos números son iguales');
-    // comprobamos si los dos números son iguales, en caso afirmativo nos lo dirá por consola.
-}else if (numero1 > numero2){
-    console.log('El nº ' + numero1 + ' es mayor que el nº ' + numero2);
-    
-}else{
-    console.log('El nº ' + numero2 + ' es mayor que el nº ' + numero1);
-}
-// en caso negativo comprobaremos si el numero1 es mayor al numero2, en caso afirmativo nos pondrá por consola que el 1r numero es mayor que el 2do, si ésta ultima condición no se cumple significa que el numero2 es el mayor y nos lo mostrará por consola.
+//Mandamos una orden de impresión por consola que haga la operación matemática de para allar el area del circulo y nos imprima el resultado.
+console.log((R * R) * Math.PI);
+
+let divweb = document.getElementById("resultado");
+
+divweb.innerHTML = "El Area del círculo és " +(R*R) * Math.PI;
