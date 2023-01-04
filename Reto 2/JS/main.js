2556fe7a06bab8747c25779c98bf7f3b8a04c006
Eduard <eduardsanguinocastro@gmail.com>
2023-01-04 12:53:53 +0100

    Se ha realizado y comprobado el reto nº 2

--- Reto 2/JS/main.js
+++ Reto 2/JS/main.js
@@ -1,18 +1,5 @@
-var nombre = prompt('Introduce tu nombre');
-var edad = Number(prompt ('Introduce tu edad')); //recordemos indicar con "NUMBER" que el resultado de un "prompt" serán datos númericos.
-var pelisPermitidas = [];
+//Creamos la variable String con el nombre del usuario
+var nombre = 'Eduard';
 
-var listaPeliculas =[
-    {titulo: "Soy Leyenda", edadMinima: 13},
-    {titulo: "Rey Leon", edadMinima: 3},
-    {titulo: "La Marca del Demonio", edadMinima: 18},
-    {titulo: "Fast & Furious", edadMinima: 8},
-]//Los array aparte de lista variables pueden ser listas de objetos como en este caso
-
-for(var i = 0 ; i < listaPeliculas.length ; i++){
-    if(edad >= listaPeliculas[i].edadMinima){
-        pelisPermitidas.push(listaPeliculas[i].titulo);//esto hace un push del titulo de la película de la array en la que se alle el programa en el momento que lee el código, sólo si la edad insertada por el usuario es igual o superior a la edad minima indicada por la película
-    }
-}//IMPORTANTE no se puede imprimir como texto un objeto, solo sus valores, por eso si queremos que al hacer un console.log de un array nos imprima cierto valor habra que hacer que el array correspondiente sea solo de variables y no de objetos.
-
-console.log(nombre + ' puede ver las siguientes películas: ' + pelisPermitidas.join('. '));
+//Mandamos una orden de imprimir en consola el mensaje "Bienvenido " y el valor de la variable con valor Sring "nombre"
+console.log('Bienvenido ' + nombre);
--- Reto 4/JS/main.js
+++ Reto 4/JS/main.js
@@ -1,18 +1,15 @@
-var nombre = prompt('Introduce tu nombre');
-var edad = Number(prompt ('Introduce tu edad')); //recordemos indicar con "NUMBER" que el resultado de un "prompt" serán datos númericos.
-var pelisPermitidas = [];
+//Creamos las variables númericas con valor fijo
+var numero1 = 15;
+var numero2 = 15;
 
-var listaPeliculas =[
-    {titulo: "Soy Leyenda", edadMinima: 13},
-    {titulo: "Rey Leon", edadMinima: 3},
-    {titulo: "La Marca del Demonio", edadMinima: 18},
-    {titulo: "Fast & Furious", edadMinima: 8},
-]//Los array aparte de lista variables pueden ser listas de objetos como en este caso
-
-for(var i = 0 ; i < listaPeliculas.length ; i++){
-    if(edad >= listaPeliculas[i].edadMinima){
-        pelisPermitidas.push(listaPeliculas[i].titulo);//esto hace un push del titulo de la película de la array en la que se alle el programa en el momento que lee el código, sólo si la edad insertada por el usuario es igual o superior a la edad minima indicada por la película
-    }
-}//IMPORTANTE no se puede imprimir como texto un objeto, solo sus valores, por eso si queremos que al hacer un console.log de un array nos imprima cierto valor habra que hacer que el array correspondiente sea solo de variables y no de objetos.
-
-console.log(nombre + ' puede ver las siguientes películas: ' + pelisPermitidas.join('. '));
+//hacemos una comparación de condición en dos pasos:
+if (numero1 == numero2){  
+    console.log('Los dos números son iguales');
+    // comprobamos si los dos números son iguales, en caso afirmativo nos lo dirá por consola.
+}else if (numero1 > numero2){
+    console.log('El nº ' + numero1 + ' es mayor que el nº ' + numero2);
+    
+}else{
+    console.log('El nº ' + numero2 + ' es mayor que el nº ' + numero1);
+}
+// en caso negativo comprobaremos si el numero1 es mayor al numero2, en caso afirmativo nos pondrá por consola que el 1r numero es mayor que el 2do, si ésta ultima condición no se cumple significa que el numero2 es el mayor y nos lo mostrará por consola.
