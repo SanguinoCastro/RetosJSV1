d5d84971c39167f2c0d737462fddc0a88d344e43
Eduard <eduardsanguinocastro@gmail.com>
2023-01-05 16:23:18 +0100

    Se ha realizado y comprobado el reto nº13

--- Reto 13/JS/main.js
+++ Reto 13/JS/main.js
@@ -1,11 +1,44 @@
-//Creamos la variable númerica cuyo valor se pedirá al usuario.
-var R = Number(prompt('Introduce el radio de la circunferencia a calcular'));
-
-//Mandamos una orden de impresión por consola que haga la operación matemática de para allar el area del circulo y nos imprima el resultado.
-console.log((R * R) * Math.PI);
-
-//Cómo extra añadimos una variable que recoja el div "resultado" de dentro del html para poder mostrar el resultado del ejercicio también en la web.
+//Creamos las 2 variable númericas cuyo valor se pedirá al usuario.
+var operando1 = Number(prompt('Introduce el primer operando'));
+var operando2 = Number(prompt('Introduce el segundo operando'));
+//Creamos la variable que almacenará el resultado de la operación que escoja el usuario.
+var resultado = 0;
+//Creamos la variable cuyo valor en String pedirá que operación quiere hacer el usuario.
+var operacion = prompt("Indica mediante el signo aritmetico que operación quieres realizar +,-,*,/,^,%");
+//Creamos la variable que crea la vinculación entre el html y el JS.
 let divweb = document.getElementById("resultado");
+//Se crea el switch que indica al programa que operación ha de hacerse dependiendo de que signo aritmético ha indicado el usuario, y devuelve el resultado en el div del html en formato "X (signo) X = X"
+switch (operacion){ 
+    case "+":
+        resultado = operando1 + operando2;        
+        console.log (operando1 +" + " + operando2 +" = " + resultado);
+        divweb.innerHTML = operando1 +" + " + operando2 +" = " +resultado;
+    break;
+    case "-":
+        resultado = operando1 - operando2;        
+        console.log (operando1 +" - " + operando2 +" = " + resultado);
+        divweb.innerHTML = operando1 +" - " + operando2 +" = " +resultado;
+    break;
+    case "*":
+        resultado = operando1 + operando2;        
+        console.log (operando1 +" x " + operando2 +" = " + resultado);
+        divweb.innerHTML = operando1 +" x " + operando2 +" = " +resultado;
+    break;
+    case "/":
+        resultado = operando1 + operando2;        
+        console.log (operando1 +" / " + operando2 +" = " + resultado);
+        divweb.innerHTML = operando1 +" / " + operando2 +" = " +resultado;
+    break;
+    case "^":
+        resultado = operando1 + operando2;        
+        console.log (operando1 +" ^ " + operando2 +" = " + resultado);
+        divweb.innerHTML = operando1 +" ^ " + operando2 +" = " +resultado;
+    break;
+    case "%":
+        resultado = operando1 % operando2 ;        
+        console.log (operando1 +" % " + operando2 +" = " + resultado);
+        divweb.innerHTML = operando1 +" % " + operando2 +" = " +resultado;
+    break;
+}
 
-//Mandamos el cálculo mátematico al div de la web.
-divweb.innerHTML = "El Area del círculo és " +(R*R) * Math.PI;
+
