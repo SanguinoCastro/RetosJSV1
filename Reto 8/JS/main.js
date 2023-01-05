61807e90e4be7f2f6aa074030fc5a49481359e87
Eduard <eduardsanguinocastro@gmail.com>
2023-01-05 10:00:22 +0100

    Se realiza y comprueba el reto nº 8

--- Reto 8/JS/main.js
+++ Reto 8/JS/main.js
@@ -1,11 +1,6 @@
-//Creamos la variable númerica cuyo valor se pedirá al usuario.
-var R = Number(prompt('Introduce el radio de la circunferencia a calcular'));
+//Usando el bucle for, añadimos todos los parametros en la misma orden, se leeria de esta manera: para el valor "i" que vale 1, si es mayor o igual a 100 se ejecutara lo que hat entre {} y luego se le sumará 1 a su valor y se repetirá el bucle hasta que el valor "i" sea mayor que 100 que entonces ya no se imprimirá ni se añadirá mas valor y el bucle finalizará. 
+for (i = 1; i <= 100; i++){
+    console.log(i);
+}
 
-//Mandamos una orden de impresión por consola que haga la operación matemática de para allar el area del circulo y nos imprima el resultado.
-console.log((R * R) * Math.PI);
-
-//Cómo extra añadimos una variable que recoja el div "resultado" de dentro del html para poder mostrar el resultado del ejercicio también en la web.
-let divweb = document.getElementById("resultado");
-
-//Mandamos el cálculo mátematico al div de la web.
-divweb.innerHTML = "El Area del círculo és " +(R*R) * Math.PI;
+//Como se puede comprobar al usar el bucle for el código queda mas limpio y reducido.
